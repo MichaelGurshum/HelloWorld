fb00d9b17e2e57cf4c20fe542a34dadcc04df0fa
Michael Gurshumov <Michaelgurshum@gmail.com>
2017-05-02 15:32:32 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Program ask for users name and outputs an Hello world my name is : ""
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+  string name;
+  cout << "Enter Your name: ";
+  cin >> name;
+  
+  cout << "Hello World, My name is " << name <<endl; 
+
+  endl;
+}
